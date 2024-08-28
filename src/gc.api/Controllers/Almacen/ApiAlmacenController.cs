b298c38f1e52b73be02562eef363621439297493
Juanjobe <juanjobe@gmail.com>
2024-08-28 14:26:39 -0300

    primeros avances sobre registracion de box

---
+++ src/gc.api/Controllers/Almacen/ApiAlmacenController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace gc.api.Controllers.Almacen
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ApiAlmacenController : ControllerBase
+    {
+    }
+}
