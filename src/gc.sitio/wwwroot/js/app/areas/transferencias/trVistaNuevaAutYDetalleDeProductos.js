534e8e968e90b3791b5f95474a2e47d336215746
geconnecterp <ge.connect.erp@gmail.com>
2024-09-27 21:49:28 -0300

    correcciones de error en grilla nueva aut TR. #8

---
+++ src/gc.sitio/wwwroot/js/app/areas/transferencias/trVistaNuevaAutYDetalleDeProductos.js
@@ -0,0 +1,25 @@
+﻿$(function () {
+	AddEventListenerToGrid("tbNuevaAutListaSucursales");
+	AddEventListenerToGrid("tbNuevaAutListaProductos");
+});
+
+function selectNuevaAutListaSucursalesRow(x) {
+}
+
+function selectNuevaAutListaProductosRow(x) {
+}
+
+function AddEventListenerToGrid(tabla) {
+	var grilla = document.getElementById(tabla);
+	if (grilla) {
+		document.getElementById(tabla).addEventListener('click', function (e) {
+			if (e.target.nodeName === 'TD') {
+				var selectedRow = this.querySelector('.selected-row');
+				if (selectedRow) {
+					selectedRow.classList.remove('selected-row');
+				}
+				e.target.closest('tr').classList.add('selected-row');
+			}
+		});
+	}
+}
