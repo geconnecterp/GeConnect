bf4be99dbd108f8e8acf7652ed56a6fab5872b5e
geconnecterp <ge.connect.erp@gmail.com>
2025-11-10 18:48:26 -0300

    avance task #103

---
+++ src/gc.infraestructura/Dtos/Financieros/Request/ConsultaLiqDeEmpleadoRequest.cs
@@ -0,0 +1,12 @@
+﻿using gc.infraestructura.Core.EntidadesComunes;
+
+namespace gc.infraestructura.Dtos.Financieros.Request
+{
+	public class ConsultaLiqDeEmpleadoRequest : BaseFilters
+	{
+		public DateTime desde { get; set; }
+		public DateTime hasta { get; set; }
+		public int? Registros { get; set; }
+		public int? Pagina { get; set; }
+	}
+}
---
+++ src/gc.sitio/Areas/Financieros/Models/LiqDeEmpConsultaYAnulacion/LiquidacionDeEmpleadoModel.cs
@@ -0,0 +1,10 @@
+﻿using gc.infraestructura.Dtos.Financieros;
+using gc.infraestructura.Dtos.Gen;
+
+namespace gc.sitio.Areas.Financieros.Models
+{
+	public class LiquidacionDeEmpleadoModel
+	{
+		public GridCoreSmart<LiqDeEmpleadoListaDto> GrillaLiqDeEmp { get; set; }
+	}
+}
