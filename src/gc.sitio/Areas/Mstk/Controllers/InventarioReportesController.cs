a62daec1948d7d50268b4fc5cfaa1237da744ef2
geconnecterp <ge.connect.erp@gmail.com>
2025-12-30 11:17:19 -0300

    Desarrollo inicial task #122

---
+++ src/gc.sitio/Areas/Mstk/Controllers/InventarioReportesController.cs
@@ -0,0 +1,59 @@
+﻿using gc.api.core.Entidades;
+using gc.infraestructura.Core.EntidadesComunes.Options;
+using gc.infraestructura.Dtos.Gen;
+using gc.sitio.Areas.Mstk.Models;
+using gc.sitio.core.Servicios.Contratos;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace gc.sitio.Areas.Mstk.Controllers
+{
+	[Area("Mstk")]
+	public class InventarioReportesController : InventarioReportesControladorBase
+	{
+		private readonly AppSettings _setting;
+		private readonly IInventarioServicio _inventarioServicio;
+		public InventarioReportesController(IOptions<AppSettings> options, IHttpContextAccessor contexto, ILogger<InventarioReportesController> logger,
+											IInventarioServicio inventarioServicio) : base(options, contexto, logger)
+		{
+			_setting = options.Value;
+			_inventarioServicio = inventarioServicio;
+		}
+
+		public IActionResult Index()
+		{
+			var model = new InventarioReporteModel();
+			try
+			{
+				var auth = EstaAutenticado;
+				if (!auth.Item1 || auth.Item2 < DateTime.Now)
+					return RedirectToAction("Login", "Token", new { area = "seguridad" });
+
+				var titulo = "REPORTES DE INVENTARIOS";
+				ViewData["Titulo"] = titulo;
+
+				CargarDatosIniciales(model);
+
+				return View();
+			}
+			catch (Exception ex)
+			{
+				RespuestaGenerica<EntidadBase> response = new()
+				{
+					Ok = false,
+					EsError = true,
+					EsWarn = false,
+					Mensaje = ex.Message
+				};
+				return PartialView("_gridMensaje", response);
+			}
+		}
+
+		#region Métodos Privados
+		private void CargarDatosIniciales(InventarioReporteModel model)
+		{
+			//Cargar datos iniciales para el modelo si es necesario
+		}
+		#endregion
+	}
+}
---
+++ src/gc.sitio/Areas/Mstk/Models/InventarioReportes/InventarioReporteModel.cs
@@ -0,0 +1,6 @@
+﻿namespace gc.sitio.Areas.Mstk.Models
+{
+	public class InventarioReporteModel
+	{
+	}
+}
