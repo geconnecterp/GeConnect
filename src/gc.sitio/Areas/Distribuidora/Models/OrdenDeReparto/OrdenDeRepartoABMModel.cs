e8be18b387ee3a399e7261dd3bac4bdd8a273ee7
geconnecterp <ge.connect.erp@gmail.com>
2026-03-06 18:15:59 -0300

    Avance de desarrollo (parcial) maquetas ABM OR task #149

---
+++ src/gc.sitio/Areas/Distribuidora/Models/OrdenDeReparto/OrdenDeRepartoABMModel.cs
@@ -0,0 +1,13 @@
+﻿using gc.infraestructura.Dtos.Gen;
+using gc.infraestructura.Dtos.Productos.OrdenDeReparto;
+using gc.infraestructura.Dtos.Productos.Pedidos;
+
+namespace gc.sitio.Areas.Distribuidora.Models.OrdenDeReparto
+{
+	public class OrdenDeRepartoABMModel
+	{
+		public OrdenDeRepartoDto OrdenDeReparto { get; set; }
+		public GridCoreSmart<PedidoEnOrdenDeRepartoDto> ListaPedidosEnOrdenDeReparto { get; set; }
+		public GridCoreSmart<PedidoListDto> ListaPedidosPendientes { get; set; }
+	}
+}
