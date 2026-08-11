6b860a107324edb8ccf098484936d7ba01f32dc4
Juan José BENZONELLI <juanjobe@gmail.com>
2026-08-11 15:25:43 -0300

    subiendo correcciones sobre Promos y Combos

--- src/gc.infraestructura/Dtos/Productos/Ofertas/ComboProductoDto.cs
+++ src/gc.infraestructura/Dtos/Productos/Ofertas/ComboProductoDto.cs
@@ -8,6 +8,8 @@
         public decimal p_pcosto { get; set; }
         public int cantidad { get; set; }
         public decimal dto_porc { get; set; }
+        public decimal dto_imp { get; set; }
+        public string up_id { get; set; } = string.Empty;
         public char activo { get; set; }
     }
 }
